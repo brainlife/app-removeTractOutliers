6957e5a6547a5b1fc5f20a814ad4acc6b727a17a
Daniel Bullock <dnbulloc@i31.red.uits.iu.edu>
2019-02-05 13:52:34 -0500

    first commit

---
+++ build.m
@@ -0,0 +1,8 @@
+addpath(genpath('/N/u/brlife/git/vistasoft'))
+addpath(genpath('/N/u/brlife/git/jsonlab'))
+addpath(genpath('/N/u/brlife/git/wma_tools'))
+addpath(genpath('/N/u/brlife/git/encode'))
+addpath(genpath('/N/u/brlife/git/mba'))
+mcc -m -R -nodisplay -d compiled   wma_removeTractOutliers_BL
+
+exit
